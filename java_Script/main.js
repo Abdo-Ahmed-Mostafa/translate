b3c65a223a80e9272a1adb3bed62cedb7f96b921
AbdelrhmanAhmed <abdoahmedd20240@gmail.com>
2024-03-08 19:16:06 +0200

    translateapp

---
+++ java_Script/main.js
@@ -0,0 +1,140 @@
+const countries = {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic",
+  "be-BY": "Bielarus",
+  "bem-ZM": "Bemba",
+  "bi-VU": "Bislama",
+  "bjs-BB": "Bajan",
+  "bn-IN": "Bengali",
+  "bo-CN": "Tibetan",
+  "br-FR": "Breton",
+  "bs-BA": "Bosnian",
+  "ca-ES": "Catalan",
+  "cop-EG": "Coptic",
+  "cs-CZ": "Czech",
+  "cy-GB": "Welsh",
+  "da-DK": "Danish",
+  "dz-BT": "Dzongkha",
+  "de-DE": "German",
+  "dv-MV": "Maldivian",
+  "el-GR": "Greek",
+  "en-GB": "English",
+  "es-ES": "Spanish",
+  "et-EE": "Estonian",
+  "eu-ES": "Basque",
+  "fa-IR": "Persian",
+  "fi-FI": "Finnish",
+  "fn-FNG": "Fanagalo",
+  "fo-FO": "Faroese",
+  "fr-FR": "French",
+  "gl-ES": "Galician",
+  "gu-IN": "Gujarati",
+  "ha-NE": "Hausa",
+  "he-IL": "Hebrew",
+  "hi-IN": "Hindi",
+  "hr-HR": "Croatian",
+  "hu-HU": "Hungarian",
+  "id-ID": "Indonesian",
+  "is-IS": "Icelandic",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "kk-KZ": "Kazakh",
+  "km-KM": "Khmer",
+  "kn-IN": "Kannada",
+  "ko-KR": "Korean",
+  "ku-TR": "Kurdish",
+  "ky-KG": "Kyrgyz",
+  "la-VA": "Latin",
+  "lo-LA": "Lao",
+  "lv-LV": "Latvian",
+  "men-SL": "Mende",
+  "mg-MG": "Malagasy",
+  "mi-NZ": "Maori",
+  "ms-MY": "Malay",
+  "mt-MT": "Maltese",
+  "my-MM": "Burmese",
+  "ne-NP": "Nepali",
+  "niu-NU": "Niuean",
+  "nl-NL": "Dutch",
+  "no-NO": "Norwegian",
+  "ny-MW": "Nyanja",
+  "ur-PK": "Pakistani",
+  "pau-PW": "Palauan",
+  "pa-IN": "Panjabi",
+  "ps-PK": "Pashto",
+  "pis-SB": "Pijin",
+  "pl-PL": "Polish",
+  "pt-PT": "Portuguese",
+  "rn-BI": "Kirundi",
+  "ro-RO": "Romanian",
+  "ru-RU": "Russian",
+  "sg-CF": "Sango",
+  "si-LK": "Sinhala",
+  "sk-SK": "Slovak",
+  "sm-WS": "Samoan",
+  "sn-ZW": "Shona",
+  "so-SO": "Somali",
+  "sq-AL": "Albanian",
+  "sr-RS": "Serbian",
+  "sv-SE": "Swedish",
+  "sw-SZ": "Swahili",
+  "ta-LK": "Tamil",
+  "te-IN": "Telugu",
+  "tet-TL": "Tetum",
+  "tg-TJ": "Tajik",
+  "th-TH": "Thai",
+  "ti-TI": "Tigrinya",
+  "tk-TM": "Turkmen",
+  "tl-PH": "Tagalog",
+  "tn-BW": "Tswana",
+  "to-TO": "Tongan",
+  "tr-TR": "Turkish",
+  "uk-UA": "Ukrainian",
+  "uz-UZ": "Uzbek",
+  "vi-VN": "Vietnamese",
+  "wo-SN": "Wolof",
+  "xh-ZA": "Xhosa",
+  "yi-YD": "Yiddish",
+  "zu-ZA": "Zulu",
+};
+
+let textArya1 = document.querySelector(".text1");
+let textArya2 = document.querySelector(".text2");
+let select1 = document.querySelector(".select1");
+let select2 = document.querySelector(".select2");
+let myBttn = document.querySelector(".mybtn");
+
+const nameContry2 = Object.keys(countries);
+
+let boda = nameContry2.map((coun) => {
+  let newContry = `<option value=${coun}>${countries[coun]}</option>`;
+  select1.innerHTML += newContry;
+  select2.innerHTML += newContry;
+});
+
+myBttn.addEventListener("click", () => {
+  fetch(
+    `https:api.mymemory.translated.net/get?q=${textArya1.value}&langpair=${select1.value}|${select2.value}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      //   console.log(data);
+
+      if (select1.value !== select2.value && textArya1.value !== "") {
+        textArya2.innerHTML = data.responseData.translatedText;
+
+      }else if(textArya1.value == ""){
+        textArya2.innerHTML = "Please type a word";
+
+      }else if(select1.value == select2.value){
+
+        textArya2.innerHTML = "Please Select Two Distinc Languages";
+
+      }
+
+      console.log(textArya2);
+    });
+});
+// console.log(select1.value);
